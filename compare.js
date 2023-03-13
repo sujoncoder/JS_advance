311f48f16aea80400ba69223fb41055fe7b42327
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-13 21:34:05 +0600

    compare done

---
+++ compare.js
@@ -0,0 +1,9 @@
+const first = 2 > 1;
+const second = 3 < 0;
+const third = 3 == 3;
+const thirdPro = 5 === "5";
+const notEqual = 3 != 3;
+
+// Now string camparison.
+const one = "e" > "t";
+console.log(first, second, third, thirdPro, notEqual, one);
