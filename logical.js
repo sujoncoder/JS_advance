e562c2f3023106319d9bb72f829908be5ad0aa46
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-13 22:06:39 +0600

    logical oparator done

---
+++ logical.js
@@ -0,0 +1,17 @@
+// OR ||
+let officeHour = 9;
+
+if (officeHour < 10 || officeHour > 18) {
+    console.log('The office is closed.');
+}
+
+
+// &&
+let hour = 12;
+let minute = 30;
+
+if (hour == 12 && minute == 30) {
+    console.log('The time is 12:30');
+}
+
+// more logical oparator.
