9df02bb3fb66a66cf08f738d420d689042f949cd
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-13 21:49:25 +0600

    if else done

---
+++ if.js
@@ -0,0 +1,16 @@
+// IF-ElSE
+const myAge = 24;
+if (myAge == 26) {
+    console.log("yes u got it.")
+} else {
+    console.log("I am wrong.")
+}
+// IF-ElSE If
+const roll = 22;
+if (roll == 33) {
+    console.log("I am right 33");
+} else if (roll == 30) {
+    console.log("Yes i am right 30");
+} else {
+    console.log("Yes I am");
+};
