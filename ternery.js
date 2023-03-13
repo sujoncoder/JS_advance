bd05d8a3209778bfe3c5c631a968c41b7b0952a1
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-13 21:54:01 +0600

    ternery done

---
+++ ternery.js
@@ -0,0 +1,4 @@
+// Ternary oparator look / worked like if else statement but too usefull.
+const don = "Sujon Sheikh";
+const result = don == "Rifat" ? "she is datal" : "Sujon is the best don";
+console.log(result);
