d1876eaed1880daaaa6e222c851f97b25296896f
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 21:11:39 +0600

    index done

---
+++ String/index.js
@@ -0,0 +1,14 @@
+// INDEXOF
+const text = "I am from bangladesh.";
+const result = text.indexOf("a");
+console.log(result);
+
+// LAST INDEX OF
+const lastIndex = "I love my country";
+const lastResult = lastIndex.lastIndexOf("love");
+console.log(lastResult);
+
+// SEARCH METHOD
+const someData = "Now i am trying to be a programmer.";
+const dataResult = someData.search("trying");
+console.log(dataResult);
