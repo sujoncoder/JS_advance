1afbfb9cf4625687ef67106189d3e2d369aec6aa
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 19:20:59 +0600

    string done

---
+++ string.js
@@ -0,0 +1,8 @@
+const devName = "SujonSheikh";
+const log = console.log;
+// log(devName)
+log(devName.length)
+
+// SPEACIAL TEXT IN THE MIDDLE.
+const randomText = "Trust me i am junior developer \"please\" I need job.";
+log(randomText)
