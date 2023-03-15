357138c2bfd0534698e75c88aa9857809c6212b8
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 20:52:50 +0600

    unicode done

---
+++ String/charCodeAt.js
@@ -0,0 +1,4 @@
+// CHARCODEAT METHOD = WHEN YOU CALL STRING INDEX NUMBER RESULT RETURN SPEACIFIC INDEX NUMBER UNICODE.
+const charCodeAtt = "Sujon Sheikh";
+const result = charCodeAtt.charCodeAt(3);
+console.log(result);
