9a947df62654e3dde583ca7d4acaeb3a92c90b96
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 19:42:36 +0600

    string replace

---
+++ stringReplace.js
@@ -0,0 +1,8 @@
+// STRING REPLACE METHOD.
+let text = "Please visit Microsoft!";
+let newText = text.replace("Microsoft", "W3Schools");
+console.log(newText);
+
+const loveText = "I love you sujon jannath tell it,s";
+const anotherLove = loveText.replace("jannath", "NurU");
+console.log(anotherLove);
