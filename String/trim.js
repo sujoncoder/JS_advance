6841a73aac0de5b6f2b0b45dd103efe588145a1f
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 20:35:38 +0600

    trim done

---
+++ String/trim.js
@@ -0,0 +1,16 @@
+let text1 = "      Hello World!      ";
+let text2 = text1.trim(); // when you no mention speacific side trim cut both space.
+
+let log = console.log;
+
+log(text2);
+
+//  TRIMSTART
+const first = " I love Bd ❤ ";
+const result = first.trimStart();
+log(result);
+
+//  TRIMEND
+const nextText = "I am muslim. ";
+const nextResult = nextText.trimEnd();
+log(nextResult); 
