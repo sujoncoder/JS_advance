135e8109acb42f4b0cd1f02d216f362ac66ee119
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-15 19:48:22 +0600

    create string folder

--- String/stringReplace.js
+++ String/stringReplace.js
@@ -6,3 +6,9 @@ console.log(newText);
 const loveText = "I love you sujon jannath tell it,s";
 const anotherLove = loveText.replace("jannath", "NurU");
 console.log(anotherLove);
+
+
+// CASE SENSITIVE.
+let textist = "Please visit Microsoft!";
+let newTextist = text.replace(/MICROSOFT/i, "W3Schools");
+console.log(newTextist);
