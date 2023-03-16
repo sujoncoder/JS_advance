45ca88bbca0665264a051a38c058516cf0db85f9
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-16 12:39:06 +0600

    done parseFloat

---
+++ Number/parseFlot.js
@@ -0,0 +1,10 @@
+// Parse Flot Method.
+const numbers = 77.77;
+console.log(parseFloat(numbers));
+
+
+const another = "10 12 13"; // always return target first number.
+console.log(parseFloat(another));
+
+const one = "hello my age is 24";
+console.log(parseFloat(one)); // return nan
---
+++ Number/valueOf.js
@@ -0,0 +1,3 @@
+// valuOf method.
+let x = 123;
+console.log(x.valueOf(1));
