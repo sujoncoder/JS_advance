d170926b4ee3743d9e621dbb4546c00cbd3a0128
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-18 19:30:19 +0600

    done slice method

---
+++ array/slice.js
@@ -0,0 +1,8 @@
+// The slice() method slices out a piece of an array into a new array.
+
+// This example slices out a part of an array starting from array element 1("Orange"):
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(3);
+console.log(fruits);
+console.log(citrus);
