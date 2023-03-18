5e04c1199575eb38077ccb7f5bd7eaa7f3641b51
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-18 21:32:04 +0600

    done filter

---
+++ array/filter.js
@@ -0,0 +1,8 @@
+const numbers = [45, 4, 9, 16, 25];
+const over18 = numbers.filter(myFunction);
+
+function myFunction(value, index, array) {
+    const result = value > 18;
+    console.log(result);
+    return result;
+}
