e85d478036d7f5a675b1e9506aae6771bded8224
sujonphero01~ <sujonsheikh.in@gmail.com>
2023-03-18 18:56:58 +0600

    done shift m ethod

---
+++ array/shift.js
@@ -0,0 +1,5 @@
+// The shift() method removes the first array element and "shifts" all other elements to a lower index.
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const fruit = fruits.shift();
+console.log(fruit);
