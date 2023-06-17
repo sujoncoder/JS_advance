15f69645f5c8107a721b8bd4bfe73c3d84d46f9f
sujonphero <sujonsheikh.in@gmail.com>
2023-06-17 08:52:33 +0600

    done this more

--- This/this.js
+++ This/this.js
@@ -1,8 +1,19 @@
+// This
+const thisKeys = () => {
+    // console.log(this);
+};
+thisKeys();
+
 const person = {
     firstName: "John",
     lastName: "Doe",
     id: 5566,
     fullName: function () {
+        console.log("this")
         return this.firstName + " " + this.lastName;
     }
 };
+const result = person.fullName();
+console.log(result);
+
+// Explain: this keyword refers global contex or window context. according to object refer parents area.
