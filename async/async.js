0e53f73421928502afed250431d2f1fdc4ef610f
sujonphero <sujonsheikh.in@gmail.com>
2023-06-23 11:45:56 +0600

    setTimeout understanding~

--- async/async.js
+++ async/async.js
@@ -1,5 +1,26 @@
 // async.
-const aAsync = () => {
-    console.log("Async");
+console.log("i am first.")
+setTimeout(function(){
+    console.log("I am second.")
+},2000);
+console.log("i am third.")
+
+
+// first
+setTimeout(myFunction, 3000);
+
+function myFunction() {
+  console.log("I love you...");
 };
-aAsync();
+
+// Or
+setTimeout(function(){
+    console.log("I love you too...")
+},4000)
+
+// Or
+setTimeout(function() { myFunction("hello"); }, 3000);
+
+function myFunction(value) {
+  console.log("I am calling from 3rd line.")
+}
